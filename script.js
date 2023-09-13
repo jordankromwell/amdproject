bebc146f3e609925dacdc50700b1b7d10762af52
jordankromwell <144951095+jordankromwell@users.noreply.github.com>
2023-09-13 16:23:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(".slider-main_component").each(function (index) {
   const swiper = new Swiper($(this).find(".swiper")[0], {
-    slidesPerView: "auto",
+    slidesPerView: 1,
     speed: 700,
     keyboard: true,
     mousewheel: {
@@ -8,6 +8,36 @@ $(".slider-main_component").each(function (index) {
     },
     freeMode: true,
     slideToClickedSlide: true,
-    spaceBetween: "2%"
+    spaceBetween: "5%"
+    breakpoints: {
+      // mobile landscape
+      480: {
+        slidesPerView: 1,
+        spaceBetween: "4%"
+      },
+      // tablet
+      768: {
+        slidesPerView: 2,
+        spaceBetween: "3%"
+      },
+      // desktop
+      992: {
+        slidesPerView: 3,
+        spaceBetween: "2%"
+      }
+  },
+  pagination: {
+    el: $(this).find(".swiper-bullet-wrapper")[0],
+    bulletActiveClass: "is-active",
+    bulletClass: "swiper-bullet",
+    bulletElement: "button",
+    clickable: true
+  },
+  scrollbar: {
+    el: $(this).find(".swiper-drag-wrapper")[0],
+    draggable: true,
+    dragClass: "swiper-drag",
+    snapOnRelease: true
+  }
   });
 });
