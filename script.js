41320eace9164b72fa4399af9dbef0170e12645a
jordankromwell <144951095+jordankromwell@users.noreply.github.com>
2023-09-13 15:42:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(".slider-main_component").each(function (index) {
   const swiper = new Swiper($(this).find(".swiper")[0], {
-    slidesPerView: "auto"
+    slidesPerView: "auto",
+    speed: 700,
+    keyboard: true,
+    mousewheel: true,
   });
 });
